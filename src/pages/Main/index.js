668ae9975d6708359a7f5c5645457f4b8a9b5db4
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-04-17 21:39:25 -0300

    Component Main e seus styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
+import styles from "./styles";
+
+import logo from "../../assets/logo.png";
 
 export default class Main extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent Main </Text>
+      <View style={styles.container}>
+        <Image style={styles.logo} height={55} width={200} source={logo} />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Crie um box"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+        />
+
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.buttonText}>Criar</Text>
+        </TouchableOpacity>
       </View>
     );
   }
